ffd452d92fe3446c27e1263726355bccbae0a833
romain.bodnar <romain.bodnar@cdbdx.biz>
2023-09-08 16:59:29 +0200

    feat: 🎸 add styled component for counter

--- src/counters/Counter/Counter.jsx
+++ src/counters/Counter/Counter.jsx
@@ -1,5 +1,8 @@
+import { Button, ButtonGroup, Card, Typography } from "@mui/material";
 import { number } from "prop-types";
+
 import { useCounter } from "../../hooks";
+import { CardContent } from "./styles";
 
 Counter.propTypes = {
   initialValue: number,
@@ -7,15 +10,19 @@ Counter.propTypes = {
 };
 
 export default function Counter({ initialValue = 0, step = 1 }) {
-  // TODO : begin with another number
-  // TODO : increment/decrement by another number
   const [counter, increment, decrement] = useCounter(initialValue, step);
 
   return (
-    <>
-      <p>{counter}</p>
-      <button onClick={increment}>+{step}</button>
-      <button onClick={decrement}>-{step}</button>
-    </>
+    <Card sx={{ maxWidth: 200, margin: "1rem" }} elevation={5}>
+      <CardContent>
+        <Typography variant="h2" component="p">
+          {counter}
+        </Typography>
+        <ButtonGroup variant="outlined">
+          <Button onClick={decrement}>-{step}</Button>
+          <Button onClick={increment}>+{step}</Button>
+        </ButtonGroup>
+      </CardContent>
+    </Card>
   );
 }
