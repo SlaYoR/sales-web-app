cb812a090faf637bdd4b73639d68245d1ef0feec
romain.bodnar <romain.bodnar@cdbdx.biz>
2023-09-12 12:04:27 +0200

    feat: 🎸 fetch products by API for HomePage data grid

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // Le strict mode appelle 2 fois les useEffects pour vérifier que les fonctions sont pures!
   <React.StrictMode>
     <App />
   </React.StrictMode>,
